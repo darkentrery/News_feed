d3832b4a9c693df22c2ea30e9dcfa8af87f6c0c5
darkentrery <badajkovmihail@gmail.com>
2022-05-03 13:54:10 +0700

    -

--- news_site/feed/forms.py
+++ news_site/feed/forms.py
@@ -1,5 +1,5 @@
 from .models import Articles
-from django.forms import ModelForm, TextInput, DateTimeInput, Textarea, ImageField
+from django.forms import ModelForm, TextInput, DateTimeInput, Textarea
 
 class ArticleForm(ModelForm):
     class Meta:
--- news_site/feed/views.py
+++ news_site/feed/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.views.generic import DetailView
 from .models import Articles
 from .forms import ArticleForm
 from django.views.generic import DetailView, ListView
